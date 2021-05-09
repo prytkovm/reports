12f581c4fbb24aa4176e21fa96738ea20d1abade
Maxim Prytkov <58343706+prytkovm@users.noreply.github.com>
2021-05-10 00:15:05 +0300

    Update

--- script/main.py
+++ script/main.py
@@ -1,109 +1,99 @@
 # -*- coding: utf-8 -*-
 import openpyxl as excel
-from datetime import date
-from datetime import timedelta
+from datetime import date, timedelta
 import os
 import random
 import shutil
 
 
-def put_base(file, exercises, flag, date_time, name, teacher_name, us_section, student_num, group, day_num):
-    current = -1
-    file['H4'] = us_section
-    file['K4'] = teacher_name
-    file['C4'] = name
-    file['B4'] = student_num
-    file['G4'] = group
+def put_base(file, exercises, flag, date_time, day_num):
+    curr = -1
     file[flag] = 'Разминка'
     if flag == 'B10':
-        file['C8'] = date_time.strftime("%d.%m.20%y")
-        file['F10'] = '15 минут'
-        file['G10'] = 1
-        file['B25'] = 'Растяжка'
-        file['F25'] = '5 минут'
-        file['G25'] = 1
-        file['G7'] = day_num
-        for i in range(11, 22):
-            iterator = 0
-            file['F' + str(i)] = '1 минута'
-            file['G' + str(i)] = 5
-            while current != 0:
-                iterator += 1
-                current = exercises['C' + str(iterator)].value
-                if iterator >= 137:
-                    iterator = 1
-                    for j in range(1, 138):
-                        exercises['C' + str(j)] = 0
-                    break
-            file['B' + str(i)] = exercises['B' + str(iterator)].value
-            exercises['C' + str(iterator)] = 1
-            current = -1
+        flag = flag.replace('B10', 'C8')
+    elif flag == 'H10':
+        flag = flag.replace('H10', 'I8')
     else:
-        file['I8'] = date_time.strftime("%d.%m.20%y")
-        file['L10'] = '15 минут'
-        file['M10'] = 1
-        file['H25'] = 'Растяжка'
-        file['L25'] = '5 минут'
-        file['M25'] = 1
-        file['M7'] = day_num
-        for i in range(11, 22):
-            iterator = 0
-            file['L' + str(i)] = '1 минута'
-            file['M' + str(i)] = 5
-            while current != 0:
-                iterator += 1
-                if iterator >= 137:
-                    iterator = 1
-                    for j in range(1, 138):
-                        exercises['C' + str(j+1)] = 0
-                    break
-                current = exercises['C' + str(iterator)].value
-            file['H' + str(i)] = exercises['B' + str(iterator)].value
-            exercises['C' + str(iterator)] = 1
-            current = -1
+        exit(-1)
+    file[flag] = date_time.strftime("%d.%m.20%y")
+    code = ord(flag[0]) + ord('F') - ord('C')
+    flag = flag.replace(flag, chr(code) + '10')
+    file[flag] = '15 минут'
+    code = ord(flag[0]) + 1
+    flag = flag.replace(flag, chr(code) + '10')
+    file[flag] = 1
+    code = ord(flag[0]) - (ord('G') - ord('B'))
+    flag = flag.replace(flag, chr(code) + '25')
+    file[flag] = 'Растяжка'
+    code = ord(flag[0]) + 4
+    flag = flag.replace(flag, chr(code) + '25')
+    file[flag] = '5 минут'
+    code = ord(flag[0]) + 1
+    flag = flag.replace(flag, chr(code) + '25')
+    file[flag] = 1
+    flag = flag.replace(flag, chr(code) + '7')
+    file[flag] = day_num
+    code = ord(flag[0]) - 1
+    flag = flag.replace(flag, chr(code))
+    iterator_header = flag[0]
+    for i in range(11, 22):
+        iterator_s = 0
+        file[iterator_header + str(i)] = '1 минута'
+        code = ord(flag[0])
+        flag = flag.replace(flag, chr(code + 1) + str(i))
+        file[flag] = 5
+        while curr != 0:
+            iterator_s += 1
+            curr = exercises['C' + str(iterator_s)].value
+            if iterator_s >= 137:
+                iterator_s = 1
+                for j in range(1, 138):
+                    exercises['C' + str(j)] = 0
+                break
+        code = ord(flag[0]) - (ord('G') - ord('B'))
+        flag = flag.replace(flag, chr(code) + str(i))
+        exercises['C' + str(iterator_s)] = 1
+        file[flag] = exercises['B' + str(iterator_s)].value
+        code = ord(flag[0])
+        flag = flag.replace(flag, chr(code + (ord('G') - ord('B')) - 1))
+        curr = -1
 
 
 def put_with_ball(file, flag, exercises):
     current = -1
-    if flag == 'B22':
-        for i in range(22, 25):
-            iterator = 0
-            while current != 0:
-                iterator += 1
-                current = exercises['G' + str(iterator)].value
-                if iterator >= 41:
-                    iterator = 1
-                    for j in range(1, 42):
-                        exercises['G' + str(j)] = 0
-                    break
+    first = flag[0]
+    for i in range(22, 25):
+        iterator = 0
+        while current != 0:
+            iterator += 1
+            current = exercises['G' + str(iterator)].value
+            if iterator >= 41:
+                iterator = 1
+                for j in range(1, 42):
+                    exercises['G' + str(j)] = 0
+                break
+        if first == 'B':
             file['B' + str(i)] = exercises['F' + str(iterator)].value
             file['F' + str(i)] = exercises['D' + str(iterator)].value
             file['G' + str(i)] = exercises['E' + str(iterator)].value
-            exercises['G' + str(iterator)] = 1
-            current = -1
-    else:
-        for i in range(22, 25):
-            iterator = 0
-            while current != 0:
-                iterator += 1
-                current = exercises['G' + str(iterator)].value
-                if iterator >= 41:
-                    iterator = 1
-                    for j in range(1, 42):
-                        exercises['G' + str(j)] = 0
-                    break
+        else:
             file['H' + str(i)] = exercises['F' + str(iterator)].value
             file['L' + str(i)] = exercises['D' + str(iterator)].value
             file['M' + str(i)] = exercises['E' + str(iterator)].value
-            exercises['G' + str(iterator)] = 1
-            current = -1
+        exercises['G' + str(iterator)] = 1
+        current = -1
 
 
 def put_analise(file, flag):
+    parameter = random.randint(1, 2)
     pulse = 65 + random.randint(3, 15)
     max_pulse = 150 + random.randint(4, 15)
-    file[flag] = 'Самочувствие в норме, немного болят мышцы, утомился,' \
+    if parameter == 1:
+        file[flag] = 'Самочувствие в норме, немного болят мышцы, утомился,' \
                  ' максимальный пульс - {}, пульс в покое - {}'.format(max_pulse, pulse)
+    else:
+        file[flag] = 'Самочувствие в норме, утомился, немного болят мышцы, ' \
+                     ' пульс в покое - {}, максимальный пульс - {}'.format(pulse, max_pulse)
 
 
 if __name__ == '__main__':
@@ -115,7 +105,8 @@ if __name__ == '__main__':
         os.mkdir(path + '\\Отчеты')
     else:
         print("Директория \"Отчеты\" успешно создана!")
-    stud_num = int(input("Номер студ. билета (Без незначащего нуля, если он есть): "))
+    counter = 1
+    stud_num = int(input("Номер студ. билета: (Без незначащего нуля) "))
     student = input("ФИО: ")
     teacher = input("ФИО преподавателя: ")
     section = input("Название секции: ")
@@ -123,7 +114,6 @@ if __name__ == '__main__':
     lambda_days = int(input("Промежуток между занятиями на неделе: "))
     between = int(input("Промежуток между крайним занятием на неделе и следующим(на след. неделе):"))
     start_day = int(input("Стартовое число занятий в семестре: "))
-    counter = 1
     start_date = date(2021, 2, start_day)
     database = 'exercises.xlsx'
     wb2 = excel.load_workbook(database)
@@ -132,8 +122,13 @@ if __name__ == '__main__':
     workbook = template.active
     for days in range(17):  # 17
         os.chdir(path + '\\Отчеты')
+        workbook['H4'] = section
+        workbook['K4'] = teacher
+        workbook['C4'] = student
+        workbook['B4'] = stud_num
+        workbook['G4'] = stud_group
         if start_date == date(2021, 4, 2):
-            put_base(workbook, ex, 'B10', start_date, student, teacher, section, stud_num, stud_group, counter)
+            put_base(workbook, ex, 'B10', start_date, counter)
             counter += 1
             put_with_ball(workbook, 'B22', ex)
             put_analise(workbook, 'B29')
@@ -141,7 +136,7 @@ if __name__ == '__main__':
             template.save(filename=finished_file)
             start_date += timedelta(days=2 * between)
         elif start_date == date(2021, 5, 9):
-            put_base(workbook, ex, 'B10', start_date, student, teacher, section, stud_num, stud_group, counter)
+            put_base(workbook, ex, 'B10', start_date, counter)
             counter += 1
             put_with_ball(workbook, 'B22', ex)
             put_analise(workbook, 'B29')
@@ -149,12 +144,12 @@ if __name__ == '__main__':
             template.save(filename=finished_file)
             start_date += timedelta(days=between)
         else:
-            put_base(workbook, ex, 'B10', start_date, student, teacher, section, stud_num, stud_group, counter)
+            put_base(workbook, ex, 'B10', start_date, counter)
             counter += 1
             put_with_ball(workbook, 'B22', ex)
             put_analise(workbook, 'B29')
             start_date += timedelta(days=lambda_days)
-            put_base(workbook, ex, 'H10', start_date, student, teacher, section, stud_num, stud_group, counter)
+            put_base(workbook, ex, 'H10', start_date, counter)
             counter += 1
             put_with_ball(workbook, 'H22', ex)
             put_analise(workbook, 'H29')
